be8df1dcefa5e176f268d2b88145fbdd608cda83
KostaLaz <44593214+KostaLaz@users.noreply.github.com>
2020-03-09 18:38:13 +0100

    Creating the models

---
+++ src/main/java/com/library/demo/domain/Admin.java
@@ -0,0 +1,27 @@
+package com.library.demo.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "admins")
+public class Admin {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String name;
+
+
+    private String password;
+
+}
---
+++ src/main/java/com/library/demo/domain/Book.java
@@ -0,0 +1,27 @@
+package com.library.demo.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Data
+@Table(name = "books")
+@AllArgsConstructor
+@NoArgsConstructor
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    private String title;
+
+    @NotNull
+    private String author;
+
+}
