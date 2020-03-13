4c94da20a581393de476ba8c9590bc10a9f65c8f
KostaLaz <44593214+KostaLaz@users.noreply.github.com>
2020-03-13 23:50:50 +0100

    API

--- src/main/java/com/library/demo/api/LibraryController.java
+++ src/main/java/com/library/demo/api/LibraryController.java
@@ -1,4 +1,25 @@
 package com.library.demo.api;
 
+import com.library.demo.service.LibraryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+
+@RestController
+@RequestMapping("/api/books")
 public class LibraryController {
+
+    @Autowired
+    private LibraryService libraryService;
+
+
+    @GetMapping("/all"){
+        public Collection<Book> books(){
+            return libraryService.findAll();
+        }
+    }
+
 }
